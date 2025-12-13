44bb4bb9a1d467f6d03ba7c1967f4e51b565a2fc
George <george.naz@gmail.com>
2025-12-13 18:17:39 +0300

    REfactoring

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+
 from PIL import Image
 
 from arguments import parse_arguments
@@ -7,6 +9,7 @@ from arguments import parse_arguments
 from database import get_database_filename, create_database
 from map_calc_tools import (
     calculate_optimal_z,
+    get_tile_4corners_gps,
     get_tile_coords_by_gps,
     get_tile_center_gps,
     find_pixel_coords,
@@ -18,14 +21,15 @@ def main():
 
     # Открываем изображение, чтобы получить размеры
     try:
-        img = Image.open(args.image_file)
-        img_width, img_height = img.size
-        img.close()
+        pil_img = Image.open(args.image_file)
+        original_width, original_height = pil_img.size
+        pil_img.close()
     except Exception as e:
         print(f"Ошибка открытия изображения: {e}")
         sys.exit(1)
 
-    # Конструируем координаты углов изображения (предполагаем прямоугольник)
+    img = {"size": {"current": {"width": original_width, "height": original_height}}}
+
     coords = {
         "top_left": (args.top_left_lat, args.top_left_lon),
         "top_right": (args.top_left_lat, args.bottom_right_lon),
@@ -35,19 +39,71 @@ def main():
 
     # Рассчитываем оптимальный zoom, если не указан
     if args.max_zoom is None:
-        max_zoom = calculate_optimal_z(img_width, img_height, coords)
+        max_zoom = calculate_optimal_z(
+            img["size"]["current"]["width"], img["size"]["current"]["height"], coords
+        )
     else:
         max_zoom = args.max_zoom
+
+    tile_top_left = {
+        "coords_tile": dict(
+            zip(
+                ("x", "y"),
+                get_tile_coords_by_gps(
+                    coords["top_left"][0], coords["top_left"][1], max_zoom
+                ),
+            )
+        ),
+        "coords_gps": {"lat": coords["top_left"][0], "lon": coords["top_left"][1]},
+    }
+
+    tile_bottom_right = {
+        "coords_tile": dict(
+            zip(
+                ("x", "y"),
+                get_tile_coords_by_gps(
+                    coords["bottom_right"][0], coords["bottom_right"][1], max_zoom
+                ),
+            )
+        ),
+        "coords_gps": {
+            "lat": coords["bottom_right"][0],
+            "lon": coords["bottom_right"][1],
+        },
+    }
+
+    img["size"]["new"] = {
+        "width": (
+            tile_bottom_right["coords_tile"]["x"]
+            - tile_top_left["coords_tile"]["x"]
+            + 1
+        )
+        * 256,
+        "height": (
+            tile_bottom_right["coords_tile"]["y"]
+            - tile_top_left["coords_tile"]["y"]
+            + 1
+        )
+        * 256,
+    }
 
     # Генерируем уникальное имя базы данных
     db_name = get_database_filename(args.image_file)
 
     # Создаем базу данных
-    create_database(db_name, max_zoom=int(max_zoom), min_zoom=0)
+    # create_database(db_name, max_zoom=int(max_zoom), min_zoom=0)
 
-    print(f"Размеры изображения: {img_width}x{img_height}")
+    print(
+        f"Исходные размеры изображения: {img['size']['current']['width']}x{img['size']['current']['height']}"
+    )
     print(f"Максимальный zoom: {max_zoom}")
     print(f"Имя базы данных: {db_name}")
+
+    print("Верхний тайл: ", tile_top_left["coords_tile"])
+    print("Нижний тайл: ", tile_bottom_right["coords_tile"])
+    print(
+        f"Размеры изображения для карты: {img['size']['new']['width']}x{img['size']['new']['height']}"
+    )
 
 
 if __name__ == "__main__":
